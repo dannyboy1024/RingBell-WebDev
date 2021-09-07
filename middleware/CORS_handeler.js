a3e51c57f041c5fb399f80c09ddbabd5630c26fb
AbigailXu <qbxzz233@gmail.com>
2021-09-06 22:02:44 -0400

    Access-Control-Allow-Origin set to accept all

--- middleware/CORS_handeler.js
+++ middleware/CORS_handeler.js
@@ -1,6 +1,7 @@
 const CROS_handeler = (req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*');
-    console.log("Access-Control-Allow-Origin set to accept all for development purpose. Disable CROS_handeler after publication.")
+    console.log("Access-Control-Allow-Origin set to accept all for development purpose. Disable CROS_handeler after publication.");
+    next();
 }
 
 module.exports = CROS_handeler;
