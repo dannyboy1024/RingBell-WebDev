fc6dcde4157c543acf9237fcac6d0241aab3777d
AbigailXu <qbxzz233@gmail.com>
2021-10-02 22:43:54 -0400

    cors

--- middleware/CORS_handeler.js
+++ middleware/CORS_handeler.js
@@ -1,9 +1,6 @@
 const CROS_handeler = (req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers', 
-        'X-Requested-With, Content-Type'
-    )
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     console.log("Access-Control-Allow-Origin set to accept all for development purpose. Disable CROS_handeler after publication.");
     next();
 }
