119f45da380daa58b5b3f0e2d197f90eb375b279
AbigailXu <qbxzz233@gmail.com>
2021-10-02 22:42:17 -0400

    modify cors

--- middleware/CORS_handeler.js
+++ middleware/CORS_handeler.js
@@ -1,8 +1,8 @@
 const CROS_handeler = (req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Access-Control-Allow-Headers', 
+        'X-Requested-With, Content-Type'
     )
     console.log("Access-Control-Allow-Origin set to accept all for development purpose. Disable CROS_handeler after publication.");
     next();
