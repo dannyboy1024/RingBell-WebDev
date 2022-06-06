48e7cbc81c0b59e0f5a3c474f9e54b3f9d145252
simulatingannealing <danny.ding@mail.utoronto.ca>
2022-06-05 20:53:12 -0400

    try 120

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,7 @@ exports.loginUser = asyncHandler(async (req, res, next) => {
   var oldTime = new Date(reToken.updated);
   var i = 0;
   
-  const timeoutLimit = 80
+  const timeoutLimit = 120
   while (correntTime.getTime() - oldTime.getTime() > 0 && i < timeoutLimit){
     reToken = await User.findOne({email: req.body.email});
     oldTime = new Date(reToken.updated);
